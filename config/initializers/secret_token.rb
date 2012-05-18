44c7d2c029a20ee1514b1b538fcc5d05c9932de1
Cyril Wei <teakscn@gmail.com>
2012-05-18 11:31:28 +1000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileCoreAcceptenceServer::Application.config.secret_token = 'd826553070984146bc9682336d9f042e6b72f4fa00c963a983b3acff88767371867139d776d4bdf7446bf10e4766ddf854d75b0f0407c987854a1155c9c75813'
